d452cb781190de4a7ff41d796a2d75352b409412
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-10 16:57:29 +0200

    Update connector_lib.py

--- receiver/connector_lib.py
+++ receiver/connector_lib.py
@@ -246,7 +246,7 @@ class SocketThread(Thread):
         
     def run(self):
         try:
-            self.sock.listen(connectorconf.MAXCONCURRENTCONNECTIONS)
+            self.sock.listen(connectorconf.MAX_CONCURRENT_CONNECTIONS)
             print('server socket opened')
 
             while True:    
