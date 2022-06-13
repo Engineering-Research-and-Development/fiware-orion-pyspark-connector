24f241ba1dd326f1bf724f35e3dda444fbe4dac7
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-13 12:25:45 +0200

    Update env_setup.sh

--- ci/env_setup.sh
+++ ci/env_setup.sh
@@ -18,6 +18,7 @@ tar xvf spark-3.2.1-bin-hadoop3.2.tgz
 mv spark-3.2.1-bin-hadoop3.2/ /opt/spark 
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
+rm spark-3.2.1-bin-hadoop3.2.tgz
 echo "installed Spark 3.2.1"
 
 echo "installing python requirements: requests"
