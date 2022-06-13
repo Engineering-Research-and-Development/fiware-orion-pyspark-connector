4bab8af152842f9815f8c589c5ae32cead95c2b8
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-13 12:56:13 +0200

    Add files via upload

---
+++ ci/raw_test/RapidPUT.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {0..100000..1}
+do
+   echo $i
+   curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "$i"
+done
+
+
