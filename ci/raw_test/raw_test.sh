c751dd1fb2f2016f24d17b096ce6f4c649aa0f4b
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-13 16:25:44 +0200

    Update raw_test.sh

--- ci/raw_test/raw_test.sh
+++ ci/raw_test/raw_test.sh
@@ -1,4 +1,10 @@
 hostname=$(hostname -I | tr ' ' '\n' | grep 10)
+
+
+sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/subscription.json
+$ curl -X POST http://localhost:1026/v2/entities/ -d @./ci/raw_test/new_entity.json --header "Content-Type: application/json"
+$ curl -X POST http://localhost:1026/v2/subscriptions/ -d @./ci/raw_test/subscription.json --header "Content-Type: application/json"
+sleep 10
 sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/connectorconf.py
 mv ./ci/raw_test/connectorconf.py ./ci/PySpark/connectorconf.py
 mv ./ci/raw_test/start.py ./ci/PySpark/
