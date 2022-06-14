1280a8ccc35b5a82237245e30c09b4146411b12d
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-14 10:42:53 +0200

    Update raw_test.sh

--- ci/raw_test/raw_test.sh
+++ ci/raw_test/raw_test.sh
@@ -7,10 +7,10 @@ sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/connectorconf.py
 mv ./ci/raw_test/connectorconf.py ./ci/PySpark/connectorconf.py
 mv ./ci/raw_test/start.py ./ci/PySpark/
 cd ./ci/PySpark/
-python3 start.py
+python3 start.py &
 #spark-submit --py-files ./ci/PySpark/start.py ./ci/PySpark/connectorconf.py ./ci/PySpark/connector_lib.py ./ci/PySpark/replier_lib.py ./ci/PySpark/replyconf.py 
 variable=$!
-sleep 60
+sleep 120
 chmod 700 ./ci/raw_test/RapidPUT.sh
 ./ci/raw_test/RapidPUT.sh > /dev/null 2> /dev/null
 sleep 10
