663dfbd25ac7bb59446faffbc26b69846a5711d3
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-14 11:25:31 +0200

    Update raw_test.sh

--- ci/raw_test/raw_test.sh
+++ ci/raw_test/raw_test.sh
@@ -7,7 +7,7 @@ sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/connectorconf.py
 mv ./ci/raw_test/connectorconf.py ./ci/PySpark/connectorconf.py
 mv ./ci/raw_test/start.py ./ci/PySpark/
 cd ./ci/PySpark/
-python3 start.py > out.txt &
+python3 start.py 1> out.txt  2> err.txt &
 #spark-submit --py-files ./ci/PySpark/start.py ./ci/PySpark/connectorconf.py ./ci/PySpark/connector_lib.py ./ci/PySpark/replier_lib.py ./ci/PySpark/replyconf.py 
 variable=$!
 cd /home/runner/work/fiware-orion-pyspark-connector/fiware-orion-pyspark-connector
@@ -16,6 +16,7 @@ echo "exit sleeping"
 chmod 700 ./ci/raw_test/RapidPUT.sh
 ./ci/raw_test/RapidPUT.sh 2> /dev/null
 sleep 20
-cat out.txt
+cat ./ci/PySpark/out.txt
+cat ./ci/PySpark/err.txt
 ps -e | grep $variable
 kill $variable
