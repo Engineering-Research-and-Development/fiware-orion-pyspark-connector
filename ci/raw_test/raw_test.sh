1a4d0b030a24f34083068edcbbb2ede6461ed4a4
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-14 12:22:22 +0200

    Update raw_test.sh

--- ci/raw_test/raw_test.sh
+++ ci/raw_test/raw_test.sh
@@ -10,7 +10,7 @@ cd /home/runner/work/fiware-orion-pyspark-connector/fiware-orion-pyspark-connect
 sleep 90
 echo "exit sleeping"
 chmod 700 ./ci/raw_test/RapidPUT.sh
-./ci/raw_test/RapidPUT.sh > 1> /dev/null 2> /dev/null
+./ci/raw_test/RapidPUT.sh 1> /dev/null 2> /dev/null
 sleep 20
 echo "reading output file"
 cat ./ci/PySpark/out.txt
