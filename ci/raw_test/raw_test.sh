a99780e8c86785511cb99f27a6f387012a3eb30a
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-14 10:23:49 +0200

    Update raw_test.sh

--- ci/raw_test/raw_test.sh
+++ ci/raw_test/raw_test.sh
@@ -2,12 +2,10 @@ hostname=$(hostname -I | tr ' ' '\n' | grep 10)
 
 
 sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/subscription.json
-cat ./ci/raw_test/subscription.json
 curl -X POST http://localhost:1026/v2/entities/ -d @./ci/raw_test/new_entity.json --header "Content-Type: application/json"  2> /dev/null
 curl -X POST http://localhost:1026/v2/subscriptions/ -d @./ci/raw_test/subscription.json --header "Content-Type: application/json" 2> /dev/null
 sleep 10
 sed -i "s/0.0.0.0/$hostname/g" ./ci/raw_test/connectorconf.py
-cat ./ci/raw_test/connectorconf.py
 mv ./ci/raw_test/connectorconf.py ./ci/PySpark/connectorconf.py
 mv ./ci/raw_test/start.py ./ci/PySpark/
 spark-submit --py-files ./ci/PySpark/start.py ./ci/PySpark/connectorconf.py ./ci/PySpark/connector_lib.py ./ci/PySpark/replier_lib.py ./ci/PySpark/replyconf.py &
@@ -16,4 +14,5 @@ sleep 60
 chmod 700 ./ci/raw_test/RapidPUT.sh
 ./ci/raw_test/RapidPUT.sh > /dev/null 2> /dev/null
 sleep 10
-
+ps -e | grep $variable
+kill $variable
