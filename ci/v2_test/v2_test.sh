08f3da14dd8129182d003afffc20ed902ca88354
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-14 12:02:11 +0200

    Create v2_test.sh

---
+++ ci/v2_test/v2_test.sh
@@ -0,0 +1,20 @@
+
+hostname=$(hostname -I | tr ' ' '\n' | grep 10)
+sed -i "s/0.0.0.0/$hostname/g" ./ci/v2_test/connectorconf.py
+mv ./ci/v2_test/connectorconf.py ./ci/PySpark/connectorconf.py
+mv ./ci/v2_test/start.py ./ci/PySpark/
+cd ./ci/PySpark/
+python3 start.py 1> out.txt  2> err.txt &
+variable=$!
+cd /home/runner/work/fiware-orion-pyspark-connector/fiware-orion-pyspark-connector
+sleep 90
+echo "exit sleeping"
+chmod 700 ./ci/v2_test/RapidPUT.sh
+./ci/v2_test/RapidPUT.sh > /dev/null
+sleep 20
+echo "reading output file"
+cat ./ci/PySpark/out.txt
+echo "reading error file"
+cat ./ci/PySpark/err.txt
+ps -e | grep $variable
+kill $variable
