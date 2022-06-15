44c25796f8e87b56cfcf0ac920b23f8bfe98eac9
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-15 12:12:33 +0200

    Update RapidPUT.sh

--- ci/v2_test/RapidPUT.sh
+++ ci/v2_test/RapidPUT.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for i in {0..10..1}
-do
-   echo $i
-   curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "$i"
-done
+#for i in {0..10..1}
+#do
+#   echo $i
+#   curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "$i"
+#done
+
+
+curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
