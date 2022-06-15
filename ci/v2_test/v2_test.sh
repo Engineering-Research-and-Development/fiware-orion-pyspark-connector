5c9e6916b8ae81d05b99487f170d28f158f5d7d0
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-15 09:06:23 +0200

    Update v2_test.sh

--- ci/v2_test/v2_test.sh
+++ ci/v2_test/v2_test.sh
@@ -3,16 +3,23 @@ hostname=$(hostname -I | tr ' ' '\n' | grep 10)
 sed -i "s/0.0.0.0/$hostname/g" ./ci/v2_test/connectorconf.py
 mv ./ci/v2_test/connectorconf.py ./ci/PySpark/connectorconf.py
 mv ./ci/v2_test/start.py ./ci/PySpark/
-mv ./ci/v2_test/Test.txt ./ci/PySpark/
+#mv ./ci/v2_test/Test.txt ./ci/PySpark/
 cd ./ci/PySpark/
 python3 start.py &
 variable=$!
+
+cat connectorconf.py
+cat start.py
+
 cd /home/runner/work/fiware-orion-pyspark-connector/fiware-orion-pyspark-connector
 sleep 30
 echo "exit sleeping"
 chmod 700 ./ci/v2_test/RapidPUT.sh
 ./ci/v2_test/RapidPUT.sh
 sleep 50
+
+
+
 echo "reading output file"
 cat ./ci/PySpark/out.txt
 rm ./ci/PySpark/out.txt
