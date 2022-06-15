2e0d8436abb7db3065665a3c997a42fe48166946
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-15 09:10:51 +0200

    Update RapidPUT.sh

--- ci/v2_test/RapidPUT.sh
+++ ci/v2_test/RapidPUT.sh
@@ -4,5 +4,5 @@ for i in {0..10..1}
 do
    echo $i
    curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "$i"
-   curl -X GET http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010
+   curl -X GET http://localhost:1026/v2/subscriptions/
 done
