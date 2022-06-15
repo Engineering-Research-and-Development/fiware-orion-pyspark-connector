5f95a9c8d0a27236bdf7a60e0ab9cbd90e14cf12
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-15 17:28:15 +0200

    Update RapidPUT.sh

--- ci/v2_test/RapidPUT.sh
+++ ci/v2_test/RapidPUT.sh
@@ -7,4 +7,4 @@
 #done
 
 
-curl -X PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
+curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
