f33679bd43e4205cb629d63e55d497029e35900b
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-16 10:54:05 +0200

    Update RapidPUT.sh

--- Test/RapidPUT.sh
+++ Test/RapidPUT.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sleep 90
+sleep 15
+curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
+sleep 5
+curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
+sleep 5
+curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
+sleep 5
+curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
+sleep 5
 curl -vX PUT http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/value -H "Content-Type: text/plain" -d "10"
