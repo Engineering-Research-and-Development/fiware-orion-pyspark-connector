0eb3c27c0c6c7f0f0cbcc608062840f461916eda
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-16 13:01:25 +0200

    Update start.py

--- Test/start.py
+++ Test/start.py
@@ -14,12 +14,12 @@ sc = SparkContext(conf=conf)
 event, ssc = connector.Prime(sc, 5 , StorageLevel.MEMORY_AND_DISK_2)
 
 event = event.flatMap(lambda x: x.entities).map(lambda x: x.attrs['price'].value)
-#response = event.map(lambda x : replier.UnstructuredReplyToBroker('{ "value" :' + str(20) +' }'))
-#response2 = event.map(lambda x: replier.SemistructuredReplyToBroker("20", '{"value" : %%TOREPLACE%% }'))
+response = event.map(lambda x : replier.UnstructuredReplyToBroker('{ "value" :' + str(20) +' }'))
+response2 = event.map(lambda x: replier.SemistructuredReplyToBroker("20", '{"value" : %%TOREPLACE%% }'))
 response3 = event.map(lambda x : replier.ReplyToBroker("20"))
 event.pprint()
-#response.pprint()
-#response2.pprint()
+response.pprint()
+response2.pprint()
 response3.pprint()
 
 
