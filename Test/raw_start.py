1e3b7e74ecf4b71b968c3c4c609d109b5d5f14a2
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-06-16 14:49:07 +0200

    Add files via upload

---
+++ Test/raw_start.py
@@ -0,0 +1,28 @@
+import connector_lib as connector
+import replier_lib as replier
+from pyspark import SparkContext, SparkConf, StorageLevel
+
+import os
+import psutil
+import time
+
+
+conf = SparkConf().setAppName("FIWARE Orion-PySpark Connector Demo").set("spark.hadoop.yarn.resourcemanager.address", "local[2]")
+sc = SparkContext(conf=conf)
+
+
+
+event, ssc = connector.Prime(sc, 10 , StorageLevel.MEMORY_AND_DISK_2)
+#event = event.flatMap(lambda x: x.entities).map(lambda x: x.attrs['price'].value)
+#response = event.map(lambda x : replier.UnstructuredReplyToBroker('{ "value" :' + str(x) +' }'))
+event.pprint()
+#response.pprint()
+
+
+ssc.start()
+
+
+try:
+	time.sleep(300)
+except Exception as e:
+	sys.exit(0)
