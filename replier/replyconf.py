fec013a6ff694579eaa5e090154491bd3e9340a0
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-09-20 09:25:30 +0200

    Delete replier directory

--- replier/replyconf.py
+++
@@ -1,7 +0,0 @@
-API_URL = "http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/"
-METHOD = "PUT" #Choose among POST, PUT or PATCH
-FIWARE_SERVICE = ""
-FIWARE_SERVICEPATH = "/"
-CONTENT_TYPE = "application/json; charset=utf-8"
-BLUEPRINTFILE = "Test.txt"
-PLACEHOLDER = "%%TOREPLACE%%"
