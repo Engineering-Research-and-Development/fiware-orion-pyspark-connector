ab9e04935e1c3c7dcfe799a409b9e5abf9383e9d
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2022-09-20 10:28:02 +0200

    Update connectorconf.py

--- ci/raw_test/connectorconf.py
+++ ci/raw_test/connectorconf.py
@@ -13,13 +13,15 @@ MAX_CONCURRENT_CONNECTIONS = 20
 
 ### REPLIER-SIDE-CONFIGURATION ###
 
+
 API_URL = "http://localhost:1026/v2/entities/urn:ngsi-ld:Product:010/attrs/price/"
 METHOD = "PUT" #Choose among POST, PUT or PATCH
-FIWARE_SERVICE = "opcua_car"
-FIWARE_SERVICEPATH = "/demo"
+FIWARE_SERVICE = ""
+FIWARE_SERVICEPATH = "/"
 CONTENT_TYPE = "application/json; charset=utf-8"
 BLUEPRINTFILE = "Test.txt"
 PLACEHOLDER = "%%TOREPLACE%%"
+
 
 
 
